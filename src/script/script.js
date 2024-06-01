de366a4e81863bd4ac6727fc01444d60d8d904c2
facecchiamarco <facecchiamarco@icloud.com>
2024-06-01 12:25:51 +0200

    Fix script throwing error in case no elements with class 'fade' were found in the DOM

--- src/script/script.js
+++ src/script/script.js
@@ -6,11 +6,12 @@ document.querySelectorAll('.fade:not(.active)').forEach(element => {
     });
 });
 
-
-window.onscroll = e => {
-    if(window.scrollY >= fadeInElements[0].yAxisOffset - (window.innerHeight / 1.2)){
-        fadeInElements[0].element.classList.add('active');
-        fadeInElements.shift();
-        if(!fadeInElements.length) window.onscroll = undefined;
+if(fadeInElements.length){
+    window.onscroll = e => {
+        if(window.scrollY >= fadeInElements[0].yAxisOffset - (window.innerHeight / 1.2)){
+            fadeInElements[0].element.classList.add('active');
+            fadeInElements.shift();
+            if(!fadeInElements.length) window.onscroll = undefined;
+        }
     }
 }
