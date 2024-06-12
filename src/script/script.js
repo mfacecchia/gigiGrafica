9affb5656b751ca87f5d240733e02578536c714a
facecchiamarco <facecchiamarco@icloud.com>
2024-06-12 21:17:58 +0200

    Update element fade functionality to automatically check for elements to show even if not scrolled

--- src/script/script.js
+++ src/script/script.js
@@ -7,11 +7,23 @@ document.querySelectorAll('.fade:not(.active)').forEach(element => {
 });
 
 if(fadeInElements.length){
-    window.onscroll = e => {
+    window.onscroll = () => {
         if(window.scrollY >= fadeInElements[0].yAxisOffset - (window.innerHeight / 1.2)){
             fadeInElements[0].element.classList.add('active');
             fadeInElements.shift();
             if(!fadeInElements.length) window.onscroll = undefined;
         }
-    }
+    };
+    window.onload = () => {
+        let maxElementReached = false;
+        while(!maxElementReached && fadeInElements.length){
+            if(window.scrollY >= fadeInElements[0].yAxisOffset - (window.innerHeight / 1.2)){
+                fadeInElements[0].element.classList.add('active');
+                fadeInElements.shift();
+                continue;
+            }
+            maxElementReached = true;
+        }
+        if(!fadeInElements.length) window.onscroll = undefined;
+    };
 }
