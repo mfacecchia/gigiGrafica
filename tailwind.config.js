0490a1bf1ea2d77300ea88d72b5eb645d87210a2
facecchiamarco <facecchiamarco@icloud.com>
2024-06-14 16:42:39 +0200

    Update tailwind default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     extend: {
       colors: {
         'graphicode_purple': '#442561',
-        'photos_brown': '#302A26'
+        'photos_brown': '#302A26',
+        'nutella_brown': '#3A0C09',
+        'nicosVape_blue': {
+          700: '#103770',
+          900: '#091D35'
+        },
+        'ideaVerde_green': {
+          700: '#0A5E34',
+          900: '#062B17'
+        }
       }
     }
   },
