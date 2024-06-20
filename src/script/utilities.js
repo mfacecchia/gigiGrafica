974f5f58b42e1295f253bea1c5a7b111f5245f7c
facecchiamarco <facecchiamarco@icloud.com>
2024-06-20 20:54:36 +0200

    Add utility function to quickly create an element with ease

--- src/script/utilities.js
+++ src/script/utilities.js
@@ -14,3 +14,23 @@ function getAssetsList(pathname, filename, extension){
         resolve(assetsList);
     });
 }
+
+function createElement(elementName, attributes = null, classes = null){
+    /*
+        * Creates an element from the given `elementName` and possible `attributes` and `classes` names
+        * The `attributes` parameter should be an Object with the attribute name as key and the relative value as Object value
+        * The `classes` parameter should be an Array with all the classes names as values
+        * Returns an instance of the chosen `elementName` element
+    */
+    const element = document.createElement(elementName);
+    if(attributes){
+        // Applying all attributes to the new element
+        for(const attributeName in attributes){
+            element.setAttribute(attributeName, attributes[attributeName]);
+        }
+    }
+    if(classes){
+        element.classList.add(...classes);
+    }
+    return element;
+}
