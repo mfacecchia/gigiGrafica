f94e0e57baf6913e751ae1bce397f79af1e365eb
facecchiamarco <facecchiamarco@icloud.com>
2024-06-20 21:10:37 +0200

    Move dialog content removal to an external function

--- src/script/fullScreenImage.js
+++ src/script/fullScreenImage.js
@@ -10,17 +10,18 @@ function displayImageFullScreen(img){
     const dialog = document.querySelector('#imageFullScreen');
     const dialogContainer = dialog.querySelector('.modal-box');
     dialog.onclose = () => {
-        dialogContainer.querySelectorAll('img, h1').forEach(element => {
-            element.remove();
-        });
+        removeManifestsDialogContent(dialogContainer);
     }
     const image = createElement('img', {src: img.src, alt: img.alt});
     const imageDescription = createElement('h1', null, null, img.alt);
-    // TODO: Place this in an external function
-    dialogContainer.querySelectorAll('img, h1').forEach(element => {
-        element.remove();
-    });
+    removeManifestsDialogContent(dialogContainer);
     dialogContainer.appendChild(image);
     dialogContainer.appendChild(imageDescription);
     dialog.showModal();
+}
+
+function removeManifestsDialogContent(dialogContainer){
+    dialogContainer.querySelectorAll('img, h1').forEach(element => {
+        element.remove();
+    });
 }
